51285fed6e2a0597fd3a0aa8d83af51170578b4d
conquieta <delmonte2305>
2025-08-11 20:10:48 +0700

    menambahkan logika switch case
    menambahkan logika perhitungan dasar

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 console.log("Hello World!");
-// I'll write some functions here
-// testing commit
+
+// const num1 = document.getElementById("firstNumber");
+let num1 = 10;
+let num2 = 5;
+const operasiMtk = "bagi";
+// const num2 = document.getElementById("secondNumber");
+// const operasiMtk = document.getElementsByClassName("numOps");
+let hasil; // Untuk menyimpan hasil perhitungan
+
+switch (operasiMtk) {
+  case "tambah":
+    hasil = num1 + num2;
+    break;
+
+  case "kurang":
+    hasil = num1 - num2;
+    break;
+
+  case "kali":
+    hasil = num1 * num2;
+    break;
+
+  case "bagi":
+    if (hasil === 0) {
+      console.error("Hasil Tidak Terdefinisi"); // nanti masukkan ke kolom hasil
+    }
+    hasil = num1 / num2;
+    break;
+
+  default:
+    console.error("Mohon masukkan angka dengan benar"); // nanti masukkan ke kolom hasil
+    break;
+}
+
+console.log(`Hasil perhitungan ${hasil}`);
