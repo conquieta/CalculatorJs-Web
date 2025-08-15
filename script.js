0ec176bce21ac214b8896462f9efc4d89445c999
conquieta <delmonte2305>
2025-08-15 16:54:37 +0700

    update code

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-console.log("Hello World!");
+document.addEventListener("DOMContentLoaded", function () {
+  /* mengunduh
+file javascript dan menampilkannya setelah merender elemen html */
+  const num1 = document.getElementById("firstNumber");
+  const num2 = document.getElementById("secondNumber");
+  const operasiMtk = document.querySelectorAll("numOps");
+  const hitung = document.getElementById("hitung");
+  const kotakJawaban = document.getElementsByClassName("hasil");
+  let hasil; // variable untuk menyimpan hasil perhitungan
 
-const num1 = document.getElementById("firstNumber");
-const num2 = document.getElementById("secondNumber");
-const operasiMtk = document.getElementsByClassName("numOps");
-const hitung = document.getElementById("hitung");
-const kotakJawaban = document.getElementsByClassName("hasil");
-let hasil; // variable untuk menyimpan hasil perhitungan
-
-switch (operasiMtk) {
-  case "tambah":
-    hasil = num1 + num2;
-    break;
-
-  case "kurang":
-    hasil = num1 - num2;
-    break;
-
-  case "kali":
-    hasil = num1 * num2;
-    break;
-
-  case "bagi":
-    if (hasil === 0) {
-      console.error("Hasil Tidak Terdefinisi"); // nanti masukkan ke kolom hasil
-    }
-    hasil = num1 / num2;
-    break;
-
-  default:
-    console.error("Mohon masukkan angka dengan benar"); // nanti masukkan ke kolom hasil
-    break;
-}
-
-operasiMtk.addEventListener(click, function () {});
+  // ganti switch operator dengan eval()
+  // switch (operasiMtk) {
+  //   case "tambah":
+  //     hasil = num1 + num2;
+  //     break;
+  //
+  //   case "kurang":
+  //     hasil = num1 - num2;
+  //     break;
+  //
+  //   case "kali":
+  //     hasil = num1 * num2;
+  //     break;
+  //
+  //   case "bagi":
+  //     if (hasil === 0) {
+  //       console.error("Hasil Tidak Terdefinisi"); // nanti masukkan ke kolom hasil
+  //     }
+  //     hasil = num1 / num2;
+  //     break;
+  //
+  //   default:
+  //     console.error("Mohon masukkan angka dengan benar"); // nanti masukkan ke kolom hasil
+  //     break;
+  // }
+});
